58ace67c3cb3190d44e85d46793ef033106ab296
wei-shen-wang <b08901051@ntu.edu.tw>
2022-11-10 11:36:22 +0800

    finish dtc and shell script on main

---
+++ runall.sh
@@ -0,0 +1,3 @@
+bash script/autoRunScripts/runFAN.sh script/fanScripts/
+bash script/autoRunScripts/averageAllTL.sh rpt > rpt/avgTL.rpt
+bash script/autoRunScripts/averageAllFC.sh rpt > rpt/avgFC.rpt
---
+++ runallgolden.sh
@@ -0,0 +1,3 @@
+bash script/autoRunScripts/runFAN_golden.sh script/fanGoldenScripts/
+bash script/autoRunScripts/averageAllTL.sh rpt/gold_rpt > rpt/gold_rpt/avgTL.rpt
+bash script/autoRunScripts/averageAllFC.sh rpt/gold_rpt > rpt/gold_rpt/avgFC.rpt
