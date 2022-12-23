d5e087048c7ccf31812acbb8de8717b4e5b94b0d
wei-shen-wang <b08901051@ntu.edu.tw>
2022-12-23 13:04:56 +0800

    replace goodeval function's cinv or ... to array call

--- rebuild_and_run.sh
+++ rebuild_and_run.sh
@@ -2,3 +2,6 @@
 make clean
 make &> makefile_output.txt
 bash script/autoRunScripts/runFAN.sh script/fanScripts
+bash script/autoRunScripts/averageAllFC.sh rpt &> FC.txt
+bash script/autoRunScripts/averageAllTL.sh rpt &> TL.txt
+bash script/autoRunScripts/averageAllRT.sh rpt &> RT.txt
