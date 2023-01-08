ab1a840a728e1447abea5bd6c5b53c909dfe24d0
wei-shen-wang <b08901051@ntu.edu.tw>
2023-01-08 17:12:41 +0800

    modify the readme and upload manual

--- rebuild_and_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-make clean
-make &> makefile_output.txt
-bash script/autoRunScripts/runFAN.sh script/fanScripts
-bash script/autoRunScripts/averageAllFC.sh rpt &> FC.txt
-bash script/autoRunScripts/averageAllTL.sh rpt &> TL.txt
-bash script/autoRunScripts/averageAllRT.sh rpt &> RT.txt
